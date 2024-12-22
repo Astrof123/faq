5eb71fbb478caa992e3ae476c061a4a06bf812be
Astrof123 <71438030+Astrof123@users.noreply.github.com>
2024-12-22 16:02:48 +0800

    Changed the algorithm completely

--- faq_bot_iu.py
+++ faq_bot_iu.py
@@ -1,26 +1,11 @@
-import os
 import json
 import numpy as np
 import tensorflow as tf
-from transformers import AutoTokenizer, TFAutoModel
-import random
+from transformers import pipeline
 
 print("Происходит загрузка приложения. Подождите немного...")
 
-tokenizer = AutoTokenizer.from_pretrained("DeepPavlov/rubert-base-cased")
-model = TFAutoModel.from_pretrained("DeepPavlov/rubert-base-cased", from_pt=True)
-
-
-def get_embedding(text):
-    inputs = tokenizer(text, return_tensors="tf", padding=True, truncation=True, max_length=512)
-    outputs = model(**inputs)
-    return outputs.last_hidden_state[:, 0, :].numpy()[0]
-
-def get_answer(question, question_embeddings, answers, model):
-    q_emb = get_embedding(question)
-    similarities = np.dot(q_emb, answer_embeddings.T)
-    best_match = np.argmax(similarities)
-    return answers[best_match] if 0 <= best_match < len(answers) else "Извини, я не знаю ответа на этот вопрос."
+pipe = pipeline("feature-extraction", model="DeepPavlov/rubert-base-cased")
 
 def load_data(filepath):
     try:
@@ -31,17 +16,26 @@ def load_data(filepath):
         print(f"Ошибка загрузки данных: {e}")
         return [], []
 
+
+def get_answer(question):
+    question_embedding = np.mean(pipe(question)[0], axis=0)
+    similarities = cosine_similarity([question_embedding], question_embeddings)[0]
+    best_match_index = np.argmax(similarities) 
+
+    similarity_threshold = 0.7
+
+    if similarities[best_match_index] >= similarity_threshold:
+      return answers[best_match_index]
+    else:
+      return "Извините, я не знаю ответа на ваш вопрос."
+
 questions, answers = load_data("university_qa.json")
 
-question_embeddings = np.array([get_embedding(q) for q in questions])
-answer_embeddings = np.array([get_embedding(a) for a in answers])
-
-loaded_model = tf.keras.models.load_model('FAQ.keras')
-
+question_embeddings = np.load("question_embeddings.npy", allow_pickle=True)
+answer_embeddings = np.load("answer_embeddings.npy", allow_pickle=True)
 
 while True:
-    user_question = input("Какой вопрос вас интересует?: ")
-    response = get_answer(user_question, questions, answers, loaded_model)
-    random_index = random.randint(0, len(response) - 1)
-    
-    print(f"Ответ: {response[random_index]}")
+    question = input("Какой вопрос вас интересует?: ")
+    response = get_answer(question)
+
+    print(f"Ответ: {response}")
